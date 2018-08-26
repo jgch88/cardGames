c8c7ea146213213d5b55f38af52d39347e9f3abc
justin <radium.laboratories@gmail.com>
2018-08-26 19:50:53 +0800

    used node child process to spawn server instance in an async manner

--- blackjackTable.frontend.test.js
+++ blackjackTable.frontend.test.js
@@ -1,14 +1,26 @@
 const puppeteer = require('puppeteer');
 const APP = 'http://localhost:4000';
+const { spawn } = require('child_process');
 
 let page;
 let browser;
+let childProcess;
 
 const USER_DATA_DIR = 'C:\\Users\\Me\\Downloads\\temp';
 const USER_DATA_DIR_WSL = '/mnt/c/Users/Me/Downloads/temp';
 
+const initServer = async () => {
+  return new Promise((resolve, reject) => {
+    childProcess = spawn('node', ['server.js']);
+
+    childProcess.stdout.on('data', data => {
+      resolve();
+    })
+  })
+}
 
 beforeAll(async () => {
+  await initServer();
 
   browser = await puppeteer.launch({
     executablePath: `chrome.exe`,
@@ -21,6 +33,7 @@ beforeAll(async () => {
 
 afterAll(() => {
   browser.close();
+  childProcess.kill();
 })
 
 
