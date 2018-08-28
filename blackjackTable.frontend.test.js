1d9674c1a2fede493b9733c6044f4b62a15beae2
justin <radium.laboratories@gmail.com>
2018-08-28 18:29:19 +0800

    added two player test, figured out how to use puppeteer dialog listener

--- blackjackTable.frontend.test.js
+++ blackjackTable.frontend.test.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const APP = 'http://localhost:4000';
 const { spawn } = require('child_process');
 
-let page;
+let pages;
 let browser;
 let childProcess;
 let dialogValue;
@@ -30,12 +30,16 @@ beforeAll(async () => {
     headless: false,
     slowMo: 80,
   });
+  await browser.newPage(); // open tab for 2nd player
+  await browser.newPage(); // open tab for 3rd player
   pages = await browser.pages();
-  pages[0].on('dialog', async dialog => {
-    await dialog.accept(dialogValue);
+  // need to do this for every tab...
+  pages.forEach(page => page.on('dialog', async dialog => {
     // this permanently creates a event listener that always responds this way
     // needed a global dialogValue which the tests can change
-  });
+    // also needed each page to have its own listener
+    await dialog.accept(dialogValue);
+  }))
 })
 
 afterAll(() => {
@@ -67,7 +71,36 @@ test('player stands, game resolves', async () => {
   dialogValue = "10"
   await pages[0].$eval('#placeBet', el => el.click());
   await pages[0].$eval('#startRound', el => el.click());
+  await pages[0].waitForSelector('#playStand');
   await pages[0].$eval('#playStand', el => el.click());
   const messageLog = await pages[0].$eval('div.messageLog', el => el.innerHTML);
   expect(messageLog).toContain('All bets resolved!');
 });
+
+test('two players join, both players stand, game resolves', async () => {
+  await pages[0].goto(APP);
+  await pages[1].goto(APP);
+
+  dialogValue = "100"
+  await pages[0].waitForSelector('#joinGame');
+  await pages[0].$eval('#joinGame', el => el.click());
+  dialogValue = "200"
+  await pages[1].waitForSelector('#joinGame');
+  await pages[1].$eval('#joinGame', el => el.click());
+  let chipsInHand = await pages[0].$eval('#chipsInHand', el => el.innerHTML);
+  expect(chipsInHand).toBe('Chips: 100');
+  chipsInHand = await pages[1].$eval('#chipsInHand', el => el.innerHTML);
+  expect(chipsInHand).toBe('Chips: 200');
+
+  await pages[0].$eval('#goToBettingState', el => el.click());
+  dialogValue = "10"
+  await pages[0].$eval('#placeBet', el => el.click());
+  dialogValue = "20"
+  await pages[1].$eval('#placeBet', el => el.click());
+
+  await pages[0].$eval('#startRound', el => el.click());
+  await pages[0].$eval('#playStand', el => el.click());
+  await pages[1].$eval('#playStand', el => el.click());
+  const messageLog = await pages[0].$eval('div.messageLog', el => el.innerHTML);
+  expect(messageLog).toContain('All bets resolved!');
+}, 7000);
