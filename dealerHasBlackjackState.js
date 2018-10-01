8ce6218315393d65908520e9e53eba3e3c849078
jgch88 <radium.laboratories@gmail.com>
2018-10-01 12:45:07 +0800

    removed emitCurrentState from dealerHasBlackjackState

--- dealerHasBlackjackState.js
+++ dealerHasBlackjackState.js
@@ -5,11 +5,8 @@ const dealerHasBlackjackState = {
     this.name = 'dealerHasBlackjackState';
     this.game = game;
     this.game.addMessageToMessageLog(`[State]: Dealer has a blackjack! Checking if players have blackjack.`);
-    this.game.emitCurrentState();
     this.checkIfPlayersHaveBlackjack();
     
-    // game.observer.render();
-    game.emitCurrentState();
     game.changeState(resolveState);
   },
   joinGame() {
@@ -26,11 +23,9 @@ const dealerHasBlackjackState = {
     this.game.bets.forEach(bet => {
       if (bet.score === 21) {
         this.game.addMessageToMessageLog(`[${bet.player.nickname}]: Blackjack!`);
-        this.game.emitCurrentState();
         bet.resolve('playerDraws', 1, this.game.dealer);
       } else {
         this.game.addMessageToMessageLog(`[${bet.player.nickname}]: No Blackjack.`);
-        this.game.emitCurrentState();
         bet.resolve('playerLoses', 1, this.game.dealer);
       }
     })
